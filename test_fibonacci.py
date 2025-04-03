f2909bb7311433862c8b7ee1a580111de5a1f416
ddamuliratina <ddamuliratina@gmail.com>
2025-04-03 22:26:31 +0300

    red again

--- test_fibonacci.py
+++ test_fibonacci.py
@@ -5,3 +5,12 @@ def test_fibonacci_of_1():
 
 def test_fibonacci_of_2():
     assert fibonacci(2) == 1
+
+def test_fibonacci_of_3():
+    assert fibonacci(3) == 1
+    
+def test_fibonacci_of_4():
+    assert fibonacci(4) == 2
+    
+def test_fibonacci_of_5():  
+    assert fibonacci(5) == 3
